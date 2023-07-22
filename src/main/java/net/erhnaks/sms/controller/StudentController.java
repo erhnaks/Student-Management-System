9c4bc8c26e0b3109af2ff29c276d42dcd17cf0ef
erhnaks <erhnaks@hotmail.com>
2023-07-22 17:07:01 +0100

    Impletemented add a new student controller

--- src/main/java/net/erhnaks/sms/controller/StudentController.java
+++ src/main/java/net/erhnaks/sms/controller/StudentController.java
@@ -1,9 +1,12 @@
 package net.erhnaks.sms.controller;
 
+import net.erhnaks.sms.entity.Student;
 import net.erhnaks.sms.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -19,6 +22,16 @@ public class StudentController {
     public String listStudents(Model model) {
         model.addAttribute("students", service.getAllStudents());
         return "students";
+    }
+
+    @GetMapping("students/new")
+    public String newStudent(Model model) {
+        // Create a student object to hold student form data.
+        Student student = new Student();
+
+        model.addAttribute("student", student);
+
+        return "created";
     }
 
 
