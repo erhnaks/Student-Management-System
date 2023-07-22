a85fa8ace099de240efcc87a9b4a24eaa737ef8b
erhnaks <erhnaks@hotmail.com>
2023-07-22 14:57:15 +0100

    Created Student Service Layer

--- src/main/java/net/erhnaks/sms/service/StudentService.java
+++ src/main/java/net/erhnaks/sms/service/StudentService.java
@@ -1,4 +1,10 @@
 package net.erhnaks.sms.service;
 
+import net.erhnaks.sms.entity.Student;
+
+import java.util.List;
+
 public interface StudentService {
+
+    List<Student> getAllStudents();
 }
--- src/main/java/net/erhnaks/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/net/erhnaks/sms/service/impl/StudentServiceImpl.java
@@ -1,8 +1,24 @@
 package net.erhnaks.sms.service.impl;
 
+import net.erhnaks.sms.entity.Student;
+import net.erhnaks.sms.repository.StudentRepository;
 import net.erhnaks.sms.service.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class StudentServiceImpl implements StudentService {
 
+    private StudentRepository repository;
 
+    public StudentServiceImpl(StudentRepository repository) {
+        super();
+        this.repository = repository;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        return repository.findAll();
+    }
 }
